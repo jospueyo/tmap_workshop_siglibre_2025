afb22c1c8e44239a504a4596e16e99ee9eb2c166
Josep Pueyo-Ros <jpueyo@icra.cat>
2025-09-10 12:49:01 +0200

    add animation rendered and as gif

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git checkout gh-pages
 git merge main
 quarto render workshop.qmd --to html -o index.html --output-dir docs
-cp maps/*.html docs
+cp maps/* docs
 git add docs/*
 git commit -m "Deploy index.html"
 git push origin gh-pages
